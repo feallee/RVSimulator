8e7de5b2854890c88039b097a19bffb622e49824
Feallee <feallee@hotmail.com>
2023-12-28 16:26:52 +0800

    验证打开文件。

--- led/build.bat
+++ led/build.bat
@@ -1,5 +1,6 @@
+pushd %~dp0
 del main.elf /q
 del main.bin /q
-riscv64-unknown-elf-gcc.exe -march=rv32i -mabi=ilp32 main.c -T main.ld --entry=main -nostdlib -o main.elf
+riscv64-unknown-elf-gcc.exe -march=rv32i -mabi=ilp32 main.c -T main.ld -nostdlib -o main.elf
 riscv64-unknown-elf-objcopy --dump-section .text=main.bin main.elf
-@rem riscv64-unknown-elf-objcopy -O binary main.elf main.bin
+explorer %~dp0
